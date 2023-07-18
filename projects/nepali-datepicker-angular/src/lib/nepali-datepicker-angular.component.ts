3baa5c270de0ca79e989a5ace6a380b9cc7debf3
Rikesh <recasechhunju@gmail.com>
2023-07-18 21:27:29 +0545

    initial setup

---
+++ projects/nepali-datepicker-angular/src/lib/nepali-datepicker-angular.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ne-datepicker',
+  template: ` <p>nepali-datepicker-angular works!</p> `,
+  styles: [],
+})
+export class NepaliDatepickerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ projects/nepali-datepicker-angular/src/lib/nepali-datepicker-angular.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { NepaliDatepickerComponent } from './nepali-datepicker-angular.component';
+
+@NgModule({
+  declarations: [NepaliDatepickerComponent],
+  imports: [],
+  exports: [NepaliDatepickerComponent],
+})
+export class NepaliDatepickerModule {}
