31a0cea6704081b4d26b23a20a937357081592d0
Rikesh <recasechhunju@gmail.com>
2023-08-22 20:42:34 +0545

    Add converter with grid view

--- projects/nepali-datepicker-angular/src/lib/nepali-datepicker-angular.module.ts
+++ projects/nepali-datepicker-angular/src/lib/nepali-datepicker-angular.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { NepaliDatepickerComponent } from './nepali-datepicker-angular.component';
 import { ToNpPipe } from './pipes/to-np.pipe';
+import { CommonModule, DatePipe } from '@angular/common';
+import { ConfigType } from './interface/interface';
 
 @NgModule({
   declarations: [ToNpPipe, NepaliDatepickerComponent],
-  imports: [],
+  imports: [CommonModule],
+  providers: [DatePipe],
   exports: [NepaliDatepickerComponent],
 })
-export class NepaliDatepickerModule {}
+export class NepaliDatepickerModule {
+  static forRoot(
+    config: ConfigType
+  ): ModuleWithProviders<NepaliDatepickerModule> {
+    return {
+      ngModule: NepaliDatepickerModule,
+      providers: [
+        {
+          provide: 'config',
+          useValue: config,
+        },
+      ],
+    };
+  }
+}
