27c0a24baadfab2d99d3ae45c88053c6cc124257
Rikesh <recasechhunju@gmail.com>
2023-08-22 20:45:18 +0545

    library implementation test

--- projects/datepicker-test/src/app/app.component.ts
+++ projects/datepicker-test/src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 
+type Language = 'en' | 'ne';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'datepicker-test';
+
+  public languageOptions = [
+    {
+      label: 'English',
+      value: 'en',
+    },
+    {
+      label: 'Nepali',
+      value: 'ne',
+    },
+  ];
+
+  public date = new Date().toISOString().split(' ')[0];
+  public dateFormat = 'yyyy/mm/dd';
+  public nepaliDate: string = '';
+  public englishDate: string = '';
+  public itNepaliDate: string = '';
+  public itEnglishDate: string = '';
+  public minDate = new Date(Date.now() - 2 * 24 * 60 * 60 * 1000);
+  public maxDate = new Date(Date.now() + 2 * 24 * 60 * 60 * 1000);
+  public primaryColor = 'blue';
+  public isDark = false;
+  public isError = false;
+  public language = this.languageOptions[0].value as Language;
+  public hasMultipleCalendarView = true;
+
+  public updateNepaliDate(date: string) {
+    this.nepaliDate = date;
+  }
+  public updateEnglishDate(date: string) {
+    this.englishDate = date;
+  }
+  public updateItNepaliDate(date: string) {
+    this.itNepaliDate = date;
+  }
+  public updateItEnglishDate(date: string) {
+    this.itEnglishDate = date;
+  }
+
+  public updateMinDate() {}
 }
--- projects/datepicker-test/src/app/app.module.ts
+++ projects/datepicker-test/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NepaliDatepickerModule } from 'nepali-datepicker-angular';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, NepaliDatepickerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
